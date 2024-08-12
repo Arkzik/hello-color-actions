356c72a7625d5ec9e35c632d8120abc9791f39a6
Arkzik <Arkzikin@gmail.com>
2024-08-12 16:10:50 +0200

    Update hello.test.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Grüzi to the World from the Switzerland");
+    expect(hello.hello()).toEqual("Grüzi to the World from Switzerland");
   });
 });
